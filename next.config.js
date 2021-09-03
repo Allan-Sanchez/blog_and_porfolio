c3dd23243405f63f7114430d02fa18bb3d9276d6
Allan-Sanchez <asanchezrixtun@gmail.com>
2021-09-02 18:50:06 -0600

    config: next config for the Image

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
-}
+  // images: {
+  //   loader: 'cloudinary',
+  //   path: 'https://res.cloudinary.com/dx9n8tsyu/',
+  // },
+  images: {
+    domains: [
+      'res.cloudinary.com'
+    ],
+},
+};
