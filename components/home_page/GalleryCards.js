60d77a94de5336ab61144dc02c5744ac8cf73aba
Allan-Sanchez <asanchezrixtun@gmail.com>
2021-09-03 15:48:36 -0600

    add: Gallery page component

---
+++ components/home_page/GalleryCards.js
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import Image from "next/image";
+
+export default function GalleryCards() {
+  const [imageList, SetImageList] = useState([
+    {
+      url: "https://res.cloudinary.com/dx9n8tsyu/image/upload/v1600718338/Screenshot_2020-09-20_exam2_1_v87tnz.png",
+      width: 325,
+      height: 200,
+      name: "Ecommerce",
+    },
+    {
+      url: "https://res.cloudinary.com/dx9n8tsyu/image/upload/v1596949151/page-1_apfedz.png",
+      width: 325,
+      height: 200,
+      name: "Cake",
+    },
+    {
+      url: "https://res.cloudinary.com/dx9n8tsyu/image/upload/v1598061973/screenshot-exam1-web-developer-2020.netlify.app-2020.08.21-20_04_16_lgill3.png",
+      width: 325,
+      height: 200,
+      name: "Task Form",
+    },
+    {
+      url: "https://res.cloudinary.com/dx9n8tsyu/image/upload/v1629847504/flutter/home_page_cus1vx.jpg",
+      width: 325,
+      height: 700,
+      name: "Cookie",
+    },
+    {
+      url: "https://res.cloudinary.com/dx9n8tsyu/image/upload/v1588006191/screenshot-todo-firebase.now.sh-2020.04.27-10_48_56_qf8lkh.png",
+      width: 325,
+      height: 200,
+      name: "CRUD House",
+    },
+    {
+      url: "https://res.cloudinary.com/dx9n8tsyu/image/upload/v1591379422/inventory_system/Screenshot_2020-05-28_Laravel_3_kpjndc.png",
+      width: 325,
+      height: 180,
+      name: "Sistema de Inverario",
+    },
+    {
+      url: "https://res.cloudinary.com/dx9n8tsyu/image/upload/v1606928946/react-proyect/tinder-clone_o4khsl.png",
+      width: 325,
+      height: 200,
+      name: "Tinder Clone",
+    },
+    // {
+    //   url: "https://res.cloudinary.com/dx9n8tsyu/image/upload/v1591379425/inventory_system/Screenshot_2020-05-28_Laravel_4_qlmrfn.png",
+    //   width: 325,
+    //   height: 180,
+    //   name: "Inventory Sistem",
+    // },
+    // {
+    //   url: "https://res.cloudinary.com/dx9n8tsyu/image/upload/v1600718336/Screenshot_2020-09-20_exam2_2_y6hea9.png",
+    //   width: 325,
+    //   height: 110,
+    //   name: "Ecommerce Stock",
+    // },
+  ]);
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5 justify-items-center">
+      {/* card 1 */}
+      {imageList.map((data, index) => (
+        <div
+          className="cardContainer h-80 w-96 p-5 bg-gray-800 rounded-2xl  cursor-pointer"
+          key={index}
+        >
+          <div className=" cardImageContainer h-52 w-full bg-gray-600 rounded-xl flex justify-center">
+            <img
+              className="object-contain"
+              width={data.width}
+              height={data.height}
+              src={data.url}
+              alt={`Description image ${data.name}`}
+            />
+          </div>
+          <h3 className="mt-3 font-bold text-xl">{data.name}.</h3>
+          <div className="w-full flex justify-end">
+            <div className="h-10 bg-yellow-600 px-5 rounded-3xl flex justify-center items-center">
+              <h4>View Project</h4>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,19 @@
 import Head from "next/head";
-import LogoGithub from "../components/svg/logo-github";
-import LogoLinkedin from "../components/svg/logo-linkedin";
-import LogoYoutube from "../components/svg/logo-youtube";
-
+// styles
+// components
+import Navigation from "../components/home_page/Navigation";
+import HeroPage from "../components/home_page/HeroPage";
+import GalleryCards from "../components/home_page/GalleryCards";
+import ContactHero from "../components/home_page/ContactHero";
+import Pagination from "../components/home_page/Pagination";
+import Menu from "../components/home_page/Menu";
+import Footer from "../components/home_page/Footer";
+// icons
+import About from "../components/home_page/About";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
         <title>Portfolio | Allan Sanchez</title>
         <meta
@@ -16,43 +23,21 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-        <nav className="flex justify-between my-5">
-          <h2 className="font-bold text-2xl">Allan Sanchez</h2>
-          <ul className="flex">
-            <li className=" font-medium mx-5 text-yellow-300">
-              <a href="#">Home</a>
-            </li>
-            <li className="font-medium mx-5">
-              <a href="#">About</a>
-            </li>
-            <li className="font-medium mx-5">
-              <a href="#">Project</a>
-            </li>
-            <li className="font-medium mx-5">
-              <a href="#">Contact</a>
-            </li>
-          </ul> 
-        </nav>
-      <main className="grid grid-cols-2">
-      <div className="">
-        <div className="pt-20">
-          <h4 className="text-xl">Hey!</h4>
-          <h1 className="w-3/4 text-5xl">I&apos;m Allan Sanchez web Developer</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque aut placeat blanditiis natus quam nobis magnam, doloribus cupiditate. Quaerat, dolore.</p>
-          <div className="py-10 flex">
-            <p>Contact with me.</p>
-            <LogoGithub className="w-10 fill-current"/> 
-            <LogoLinkedin className="w-10 fill-current"/>
-            <LogoYoutube className="w-10 fill-current"/>
-            
+      <Navigation />
+      <main>
+        <HeroPage />
 
-          </div>
+        <About />
 
-        </div>
-      </div>
-      <div className="bg-red-200 h-full">image</div>
+        <section className="mt-28">
+          <Menu />
+
+          <GalleryCards />
+          <Pagination />
+          <ContactHero />
+        </section>
       </main>
-    </div>
+      <Footer />
+    </>
   );
 }
-
