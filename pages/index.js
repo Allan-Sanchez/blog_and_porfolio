ceae78f957e2f1e47d412d0534f1b8cd77956e2f
Allan-Sanchez <asanchezrixtun@gmail.com>
2021-11-19 13:36:46 -0600

    feat: metadescription and tag for seo add

--- pages/index.js
+++ pages/index.js
@@ -27,12 +27,31 @@ export default function Home({ posts }) {
   return (
     <>
       <Head>
-        <title>Portfolio | Allan Sanchez</title>
+        <title>Allan Sanchez | Portfolio, Frontend Developer, React, Vue</title>
         <meta
           name="description"
-          content="Portfolio by Allan Sanchez, Web Developer"
+          content="Portfolio, cv, resume, web projects react, vue, node, graphql also you will find mobile projects developed in flutter."
         />
         <link rel="icon" href="/favicon.ico" />
+        <meta property="og:title" content="Allan Sanchez | Portfolio, Frontend Developer, React, Vue " />
+        <meta property="og:site_name" content="blog-and-portfolio" />
+        <meta
+          property="og:url"
+          content="https://blog-and-portfolio.vercel.app/"
+        />
+        <meta
+          property="og:description"
+          content="Portfolio, CV, summary, web projects react, vue, node, graphql also you will find mobile projects developed in flutter."
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/dx9n8tsyu/image/upload/v1636494362/IMG_20211106_105652-removebg-preview_k4sd89.png"
+        />
+        <meta name="twitter:card" content="summary"></meta>
+        {/* TODO:: change domain */}
+        <meta name="twitter:site" content="@blog-and-portfolio"/>
+        <meta name="twitter:creator" content="@AllanSanchez2"/>
       </Head>
 
       <Navigation />
